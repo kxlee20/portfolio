861da0b5eb7fd546819889ef0c812af6021d5af4
kxlee <kxlee@umich.edu>
2022-07-13 23:59:47 -0400

    fixed pipeline image

--- src/constants/images.js
+++ src/constants/images.js
@@ -9,8 +9,6 @@ import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
-import mu5 from '../assets/mu5.png';
-import node from '../assets/node.png';
 import python from '../assets/python.png';
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
@@ -27,14 +25,13 @@ import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
-import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
 import asus from '../assets/asus.png';
 import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 import pipeline from '../assets/Pipeline.png';
+import pipeline2 from '../assets/Pipeline2.png';
 
 export default {
   email,
@@ -48,8 +45,6 @@ export default {
   graphql,
   html,
   javascript,
-  mu5,
-  node,
   python,
   react,
   redux,
@@ -63,12 +58,11 @@ export default {
   profile,
   circle,
   logo,
-  adidas,
   amazon,
   asus,
   bolt,
-  nb,
   skype,
   spotify,
-  pipeline
+  pipeline,
+  pipeline2
 };
--- src/container/Mutation/Mutation.jsx
+++ src/container/Mutation/Mutation.jsx
@@ -36,9 +36,9 @@ const Mutation = () => {
               de novo mutations of sets of rare disease patients able to identify pathogenic mutations in human disease patients
               with high accuracy. 
             </div>
-            {/* <div className='app__mutation-image'>
-              <img src={images.pipeline} alt="pipeline" /> 
-            </div> */}
+            <div className='app__mutation-image'>
+              <img src={images.pipeline2} alt="pipeline" /> 
+            </div>
           {/* </div> */}
         </div>
       </div>
