27d6e1edeabd8ce8685ce2d1c9106bd720438e80
kxlee <kxlee@umich.edu>
2022-07-19 03:44:15 -0400

    scroll to top auto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ScrollTop from "./components/ScrollTop";
 
 import App from './App';
 // import Projects from './container/Projects/Projects';
@@ -8,5 +9,6 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
     <BrowserRouter>
+    <ScrollTop />
     <App />
     </BrowserRouter>, document.getElementById('root'));
